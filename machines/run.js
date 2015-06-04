7f9d2529d2f561bbfed00fe5faa05849604af5d1
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-06-03 18:10:04 -0700

    removed logs

--- machines/run.js
+++ machines/run.js
@@ -1,3 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+var google = require('googleapis');
+
 module.exports = {
 
 
@@ -7,7 +12,7 @@ module.exports = {
   description: 'Run PageSpeed analizer',
 
 
-  cacheable: false,
+  cacheable: true,
 
 
   sync: false,
@@ -17,7 +22,46 @@ module.exports = {
 
 
   inputs: {
+    key: {
+      example: 'YOUR_API_KEY',
+      description: 'Your API key'
+    },
 
+    url: {
+      example: 'http://google.com',
+      description: 'URL of the resource you need to analize',
+      required: true
+    },
+
+    filterThirdPartyResources: {
+      example: true,
+      description: 'Indicates if third party resources should be filtered out before PageSpeed analysis'
+    },
+
+    locale: {
+      example: 'en',
+      description: 'The locale used to localize formatted results'
+    },
+
+    screenshot: {
+      example: true,
+      description: 'Indicates if binary data containing a screenshot should be included'
+    },
+
+    rule: {
+      example: 'test',
+      description: 'A PageSpeed rule to run; if none are given, all rules are run'
+    },
+
+    fields: {
+      example: 'formattedResults,id,invalidRules,kind,pageStats,responseCode,ruleGroups,screenshot,title,version/major,version/minor',
+      description: 'Selector specifying which fields to include in a partial response'
+    },
+
+    key: {
+      example: 'YOUR_API_KEY',
+      description: 'Your API Key'
+    }
   },
 
 
@@ -26,16 +70,15 @@ module.exports = {
     success: {
       variableName: 'result',
       description: 'Done.',
-    },
+    }
 
   },
 
 
-  fn: function (inputs,exits
-  /**/) {
+  fn: function(inputs, exits) {
+    var params = {};
+    _.merge(params, inputs);
     return exits.success();
-  },
-
-
+  }
 
 };
